6d01d3bfff957f47b45590ea27f7cf5682af4d89
Matthew Little <zone117x@gmail.com>
2014-01-07 03:39:25 -0500

    updated

--- node_modules/scrypt/build/Release/.deps/Release/scrypt.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/scrypt.node := rm -rf "Release/scrypt.node" && cp -af "Release/obj.target/scrypt.node" "Release/scrypt.node"
--- node_modules/scrypt/build/Release/.deps/Release/scrypt_lib.a.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/scrypt_lib.a := rm -rf "Release/scrypt_lib.a" && cp -af "Release/obj.target/scrypt_lib.a" "Release/scrypt_lib.a"
